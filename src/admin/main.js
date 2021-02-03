6aca05a9d96247162ce2503a5b947e984bdeb25d
侯建光 <houjianguang@enn.cn>
2021-02-03 17:24:00 +0800

    修改菜单导航

--- src/admin/main.js
+++ src/admin/main.js
@@ -1,4 +1,5 @@
 import React, {Component} from 'react'
+import {Route, Switch, Link, Redirect} from 'react-router-dom'
 import {Layout, Menu} from 'antd'
 import {
     AppstoreOutlined,
@@ -10,20 +11,25 @@ import {
     UploadOutlined,
     VideoCameraOutlined,
 } from '@ant-design/icons';
+import App from '../App'
+import AdminMenu from '../views/AdminMenu'
+
 
 class Main extends Component {
+    renderMessage = (msg) => <h5 className="bg-info text-white m-2 p-2">{ msg }</h5>
     render() {
         const {Header, Content, Footer, Sider} = Layout;
+        const { SubMenu } = Menu;
         return (
             <Layout>
-                <Header style={{ position: 'fixed', zIndex: 1, width: '100%' }}>
+                {/*<Header style={{ position: 'fixed', zIndex: 1, width: '100%' }}>
                     <div className="logo" />
-                    <Menu theme="dark" mode="horizontal" defaultSelectedKeys={['2']}>
-                        <Menu.Item key="1">nav 1</Menu.Item>
-                        <Menu.Item key="2">nav 2</Menu.Item>
-                        <Menu.Item key="3">nav 3</Menu.Item>
-                    </Menu>
-                </Header>
+                    <AdminMenu theme="dark" mode="horizontal" defaultSelectedKeys={['2']}>
+                        <AdminMenu.Item key="1">nav 1</AdminMenu.Item>
+                        <AdminMenu.Item key="2">nav 2</AdminMenu.Item>
+                        <AdminMenu.Item key="3">nav 3</AdminMenu.Item>
+                    </AdminMenu>
+                </Header>*/}
                 <Sider
                     style={{
                         overflow: 'auto',
@@ -33,132 +39,27 @@ class Main extends Component {
                     }} collapsible={true}
                 >
                     <div className="logo"/>
-                    <Menu theme="dark" mode="inline" defaultSelectedKeys={['4']}>
-                        <Menu.Item key="1" icon={<UserOutlined/>}>
-                            nav 1
+                    <Menu theme="dark" mode="inline" defaultSelectedKeys={['1']}>
+                        <Menu.Item key="1">
+                            <Link to="/main">主页</Link>
                         </Menu.Item>
-                        <Menu.Item key="2" icon={<VideoCameraOutlined/>}>
-                            nav 2
+                        <SubMenu key="menu" title="菜单管理">
+                        <Menu.Item key="menu-add">
+                            <Link to="/menu"> 新建菜单</Link>
                         </Menu.Item>
-                        <Menu.Item key="3" icon={<UploadOutlined/>}>
-                            nav 3
-                        </Menu.Item>
-                        <Menu.Item key="4" icon={<BarChartOutlined/>}>
-                            nav 4
-                        </Menu.Item>
-                        <Menu.Item key="5" icon={<CloudOutlined/>}>
-                            nav 5
-                        </Menu.Item>
-                        <Menu.Item key="6" icon={<AppstoreOutlined/>}>
-                            nav 6
-                        </Menu.Item>
-                        <Menu.Item key="7" icon={<TeamOutlined/>}>
-                            nav 7
-                        </Menu.Item>
-                        <Menu.Item key="8" icon={<ShopOutlined/>}>
-                            nav 8
-                        </Menu.Item>
+                        </SubMenu>
                     </Menu>
                 </Sider>
                 <Layout className="site-layout" style={{marginLeft: 200}}>
                     <Header className="site-layout-background" style={{padding: 0}}/>
                     <Content style={{margin: '24px 16px 0', overflow: 'initial'}}>
                         <div className="site-layout-background" style={{padding: 24, textAlign: 'center'}}>
-                            ...
-                            <br/>
-                            Really
-                            <br/>
-                            ...
-                            <br/>
-                            ...
-                            <br/>
-                            ...
-                            <br/>
-                            long
-                            <br/>
-                            ...
-                            <br/>
-                            ...
-                            <br/>
-                            ...
-                            <br/>
-                            ...
-                            <br/>
-                            ...
-                            <br/>
-                            ...
-                            <br/>
-                            ...
-                            <br/>
-                            ...
-                            <br/>
-                            ...
-                            <br/>
-                            ...
-                            <br/>
-                            ...
-                            <br/>
-                            ...
-                            <br/>
-                            ...
-                            <br/>
-                            ...
-                            <br/>
-                            ...
-                            <br/>
-                            ...
-                            <br/>
-                            ...
-                            <br/>
-                            ...
-                            <br/>
-                            ...
-                            <br/>
-                            ...
-                            <br/>
-                            ...
-                            <br/>
-                            ...
-                            <br/>
-                            ...
-                            <br/>
-                            ...
-                            <br/>
-                            ...
-                            <br/>
-                            ...
-                            <br/>
-                            ...
-                            <br/>
-                            ...
-                            <br/>
-                            ...
-                            <br/>
-                            ...
-                            <br/>
-                            ...
-                            <br/>
-                            ...
-                            <br/>
-                            ...
-                            <br/>
-                            ...
-                            <br/>
-                            ...
-                            <br/>
-                            ...
-                            <br/>
-                            ...
-                            <br/>
-                            ...
-                            <br/>
-                            ...
-                            <br/>
-                            ...
-                            <br/>
-                            ...
-                            <br/>
-                            content
+                            <Switch>
+                                <Route exac path="/main" component={App}/>
+                                <Route path="/menu" component={AdminMenu}/>
+                                <Redirect from="/" to="/main"/>
+                                <Redirect to="/main"/>
+                            </Switch>
                         </div>
                     </Content>
                     <Footer style={{textAlign: 'center'}}>Ant Design ©2018 Created by Ant UED</Footer>
